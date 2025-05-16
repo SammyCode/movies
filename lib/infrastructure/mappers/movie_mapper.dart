b81a345b6b24c686c9f860d9e747836daeb8c75e
Sam <sam.tepec.dev@gmail.com>
2025-05-16 10:14:27 -0600

    Implemented movie repository

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -15,7 +15,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
           ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-          : 'https://m.media-amazon.com/images/I/61+bW9r45LL._AC_UF894,1000_QL80_.jpg',
+          : 'no-poster',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
